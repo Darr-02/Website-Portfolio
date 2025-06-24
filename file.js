9e09b89c537ce2c296714be5a439b9b8183cef6d
Darr-02 <darrickuntarman@gmail.com>
2025-06-24 23:08:16 +0800

    Updated file

--- file.js
+++ file.js
@@ -25,12 +25,12 @@ async function typeWriter() {
 
 // For tab(skill section)
 function showContent(tabName){
-   document.querySelectorAll('.tab').forEach(function(tab){ tab.classList.remove('active')});
+  document.querySelectorAll('.tab').forEach(function(tab){ tab.classList.remove('active')});
 
-   document.querySelectorAll('.section').forEach(content => content.classList.remove('active'));
+  document.querySelectorAll('.section').forEach(content => content.classList.remove('active'));
 
-   document.getElementById(tabName).classList.add('active');
-   event.target.classList.add('active');
+  document.getElementById(tabName).classList.add('active');
+  event.target.classList.add('active');
 }
 
 window.onload = function() {
@@ -53,6 +53,35 @@ document.addEventListener("click", function(event) {
   }
 });
 
+//active navbar border
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('nav ul li a');
+
+window.addEventListener('scroll', () => {
+  let scrollMiddle = window.scrollY + window.innerHeight / 2;
+  let currentId = "";
+
+  [...sections].reverse().forEach(sec => {
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (scrollMiddle >= offset && scrollMiddle < offset + height) {
+      currentId = id;
+    }
+  });
+
+  if (currentId === "") {
+    currentId = "home";
+  }
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === `#${currentId}`) {
+      link.classList.add('active');
+    }
+  });
+});
 
 const swiper = new Swiper('.swiper', {
   cssMode: true,
